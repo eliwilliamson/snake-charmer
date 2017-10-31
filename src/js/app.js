00a204000416ffc69502cf087b50c3f1896f917f
eliwilliamson <eliwilliamson@gmail.com>
2017-10-31 00:22:39 -0400

    add thank you page and fix form

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
+$('.navbar-collapse a').on('click',function(){
+  $(".navbar-collapse").collapse('hide');
+});
+
 function rotateHeadline() {
   //set animation timing
   var animationDelay = 2500,
@@ -154,29 +158,3 @@ function rotateHeadline() {
 };
 
 $(document).ready(rotateHeadline());
-
-
-$("#contact").submit(function (e) {
-  e.preventDefault();
-  var name = $("#cf-name").val();
-  var email = $("#cf-email").val();
-  var subject = $("#cf-subject").val();
-  var message = $("#cf-message").val();
-  var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
-
-  function isValidEmail(emailAddress) {
-    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    return pattern.test(emailAddress);
-  };
-  if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
-    var $form = $(this);
-    $.post($form.attr("action"), $form.serialize()).then(function() {
-      alert("Thank you!");
-    });
-  }
-  else {
-    $('.text-danger').fadeIn(1000);
-    $('.text-success').fadeOut(500);
-  }
-  return false;
-});
